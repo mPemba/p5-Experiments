d0594384583f5bdb269c7ea1e1bd8f527ec7af3d
mPemba <michael.j.say@gmail.com>
2023-05-02 13:50:04 -0600

    drawing nice green and blue circles on the canvas

--- src/sketch.js
+++ src/sketch.js
@@ -7,30 +7,45 @@ const Sketch = () => {
   useEffect(() => {
     const sketch = (p) => {
       let circles = [];
+      let minDiameter = 10;
+      let maxDiameter = 100;
 
+      // get screen size
+      let screenWidth = window.innerWidth;
+    
       p.setup = () => {
-        const canvas = p.createCanvas(600, 600);
-        canvas.parent(canvasRef.current);
+        p.createCanvas(parseInt(screenWidth), 800);
+        p.colorMode(p.HSB, 360, 100, 100, 1);
       };
-
+    
       p.draw = () => {
-        p.background(220);
-
-        circles.forEach((circle) => {
-          p.noStroke();
+        p.background(190,56,21);
+    
+        // Draw all the circles in the array
+        for (let circle of circles) {
           p.fill(circle.color);
-          p.circle(circle.x, circle.y, circle.size);
-        });
-      };
-
-      p.mouseClicked = () => {
-        const circle = {
-          x: p.mouseX,
-          y: p.mouseY,
-          size: 20,
-          color: p.color(p.random(255), p.random(255), p.random(255)),
-        };
-        circles.push(circle);
+          p.stroke(0);
+          p.ellipse(circle.x, circle.y, circle.diameter, circle.diameter);
+        }
+    
+        // Calculate the hue value based on the mouse x-position
+        let hue = p.map(p.mouseX, 0, p.width, 120, 210);
+    
+        // Calculate the saturation value based on the mouse y-position
+        let saturation = p.map(p.mouseY, 0, p.height, 50, 100);
+    
+        // Calculate the diameter of the circle based on the mouse speed
+        let speed = p.abs(p.mouseX - p.pmouseX) + p.abs(p.mouseY - p.pmouseY);
+        let diameter = p.map(speed, 0, 20, minDiameter, maxDiameter);
+    
+        // Add a new circle to the array
+        let color = p.color(hue, saturation, 50, 0.5);
+        circles.push({ x: p.mouseX, y: p.mouseY, diameter, color });
+    
+        // Remove the oldest circle from the array if it gets too big
+        if (circles.length > 500) {
+          circles.shift();
+        }
       };
     };
 
